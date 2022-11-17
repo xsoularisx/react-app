5f18dfe1f0cb17d647ee58893cb4c20400b82e55
albert <xsoularisx@gmail.com>
2022-11-17 10:54:11 +0300

    init: lesson 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react/jsx-pascal-case */
+import React from 'react';
+import './App.scss';
+import Message_Item from './components/Message_Item/Message_Item';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Message_Item name='Albert' />
     </div>
   );
 }
---
+++ src/components/Message_Item/Message_Item.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Message_Item(props) {
+    return (
+        <div>
+            <h1>My name is {props.name}, this is my first React App</h1>
+        </div>
+    );
+}
+
+export default Message_Item;
